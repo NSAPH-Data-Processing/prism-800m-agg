5390436a2955ec254b008cf7e42fc96f579384b9
shreyanalluri <nallshrey@gmail.com>
2026-04-23 10:17:56 -0400

    run step 02 for 2000-2016

--- code/02c_PRISM_Raster_Extract_Exe_v03.sh
+++ code/02c_PRISM_Raster_Extract_Exe_v03.sh
@@ -6,25 +6,6 @@
 #   (1) chmod +x 02c_PRISM_Raster_Extract_Exe_v03.sh
 #   (2) ./02c_PRISM_Raster_Extract_Exe_v03.sh
 
-sbatch --array=198101-198149 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198201-198249 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198301-198349 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198401-198449 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198501-198549 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198601-198649 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198701-198749 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198801-198849 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=198901-198949 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199001-199049 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199101-199149 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199201-199249 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199301-199349 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199401-199449 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199501-199549 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199601-199649 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199701-199749 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199801-199849 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=199901-199949 ./02b_PRISM_Raster_Extract_v03.sh
 sbatch --array=200001-200049 ./02b_PRISM_Raster_Extract_v03.sh
 sbatch --array=200101-200149 ./02b_PRISM_Raster_Extract_v03.sh
 sbatch --array=200201-200249 ./02b_PRISM_Raster_Extract_v03.sh
@@ -42,10 +23,3 @@ sbatch --array=201301-201349 ./02b_PRISM_Raster_Extract_v03.sh
 sbatch --array=201401-201449 ./02b_PRISM_Raster_Extract_v03.sh
 sbatch --array=201501-201549 ./02b_PRISM_Raster_Extract_v03.sh
 sbatch --array=201601-201649 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=201701-201749 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=201801-201849 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=201901-201949 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=202001-202049 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=202101-202149 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=202201-202249 ./02b_PRISM_Raster_Extract_v03.sh
-sbatch --array=202301-202349 ./02b_PRISM_Raster_Extract_v03.sh
